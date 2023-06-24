d316f0f34fd5d586e0c25f7df1ffae95948a0687
Gabriel Sánchez <gasvil17@gmail.com>
2023-06-24 13:50:02 -0500

    feath: home 90%

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 importScript('./scripts/layout.js') // Sólo para desarrollo de maqueta
+importScript('./scripts/glide.min.js')
 importScript('./scripts/utils.js')
 
-const url = window.location.href
+const url = window.location.pathname
 
-if(url.includes('bonus')) {
+if(url.includes('index')) {
+  importScript('./scripts/home.js')
+} else if(url.includes('bonus')) {
   importScript('./scripts/bonus.js')
 } else if(url.includes('profile')) {
   importScript('./scripts/profile.js')
+} else {
+  importScript('./scripts/home.js')
 }
